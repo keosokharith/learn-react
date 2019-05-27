80fa454fe69b036e24bcc623cbc68926cc52139f
keosokharith <keo.sokharith@yahoo.com>
2019-05-27 15:18:47 +0700

    testing route

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> ទំព័រដើម </Link></li>
+            <li><Link to={'/contact'} className="nav-link">​​ ទំនាក់ទំនង </Link></li>
+            <li><Link to={'/about'} className="nav-link">​ អំពីយើង </Link></li>
+            <li><Link to={'/about1'} className="nav-link">​ test1 </Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/about' component={About} />
+              <Route path='/about1' component={Home} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
